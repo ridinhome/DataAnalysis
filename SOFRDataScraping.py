2d9aebc7e2c860b2497e95d6fb9acb7e704581b6
Kayam Rajaram <ridinhome@Kayams-MacBook-Pro.local>
2021-06-01 21:42:11 -0400

    Update to include start and end dates

--- SOFRDataScraping.py
+++ SOFRDataScraping.py
@@ -1,9 +1,14 @@
 import pandas as pd
 
-def fredrequests(series,api):
+def fredrequests (series,api,startdate,enddate):
     name = series
     key = api
-    url = ("https://api.stlouisfed.org/fred/series/observations?series_id="+name+"&realtime_end=9999-12-31&api_key="+key+"&file_type=json")
+    start = startdate
+    end = enddate
+    url = ("https://api.stlouisfed.org/fred/series/observations?series_id="+name+"&realtime_start="
+           +start
+           +"&realtime_end="+end
+           +"&api_key="+key+"&file_type=json")
     df = pd.read_json (url)
     new_df = df['observations'].apply(pd.Series)
     new_df.drop(['realtime_start', 'realtime_end'], axis=1, inplace=True)
