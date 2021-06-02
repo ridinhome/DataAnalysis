4063ca55d699844f22278a18a590b21f646ec66e
Kayam Rajaram <ridinhome@Kayams-MacBook-Pro.local>
2021-06-01 22:18:20 -0400

    Added docstrings

--- SOFRDataScraping.py
+++ SOFRDataScraping.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
-def fredrequests (series,api,enddate="9999-12-31",startdate="1776-07-04"):
+def fredrequests (series,api,startdate="1776-07-04",enddate="9999-12-31"):
+    """Returns a data series from the FRED database using the JSON API. Pass start date and end-date
+    in yyyy-mm-dd format. Leaving out start date and end date defaults to full series."""
     name = series
     key = api
     start = startdate
